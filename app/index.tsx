7080362d62de0374d360c923c1f81fcdc61761aa
ricoglr <rico.dev.tr@gmail.com>
2024-09-24 13:20:33 +0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import CreateScreen from './screens/CreateScreen';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer independent={true}>
+      <Stack.Navigator screenOptions={{headerTitle:'Blog Uygulamasi'}}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="CreateScreen" component={CreateScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
