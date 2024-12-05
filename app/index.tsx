3484bd4ae9af81a5e95124c5e1e15992691d9e98
ricoglr <rico.dev.tr@gmail.com>
2024-12-05 15:56:05 +0300

    ikonlar ile yönlendirmeler

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,39 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import CreateScreen from './screens/CreateScreen';
 import { Provider } from '@/context/BlogContext';
-import ShowScreen from './screens/ShowScreen';
 const Stack = createNativeStackNavigator();
+import Entypo from '@expo/vector-icons/Entypo';
+import EditScreen from './screens/EditScreen';
+import ShowScreen from './screens/ShowScreen';
+import Feather from '@expo/vector-icons/Feather';
 
 function App() {
   return (
     <Provider>
     <NavigationContainer independent={true}>
       <Stack.Navigator screenOptions={{headerTitle:'Blog Uygulamasi'}}>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} options={({navigation})=>({
+          headerRight:()=>(
+            <TouchableOpacity onPress={()=>navigation.navigate('CreateScreen')}>
+              <Entypo name="plus" size={24} color="black" />
+            </TouchableOpacity>
+          )
+        })}/>
         <Stack.Screen name="CreateScreen" component={CreateScreen} />
-        <Stack.Screen name="Show" component={ShowScreen} />
+        <Stack.Screen name="Show" component={ShowScreen}  options={({navigation})=>({
+          headerRight:()=>(
+            <TouchableOpacity onPress={()=>navigation.navigate('Edit')}>
+              <Feather name="edit" size={24} color="black" />
+            </TouchableOpacity>
+          )
+        })}/>
+        <Stack.Screen name="Edit" component={EditScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
     </Provider>
