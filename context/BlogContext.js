d7387a8cabd4bb0946ee723a4af7e02ef7a1c874
ricoglr <rico.dev.tr@gmail.com>
2024-12-05 14:03:30 +0300

    useReducer ile ekleme blog ekleme işlemi

--- context/BlogContext.js
+++ context/BlogContext.js
@@ -1,13 +1,24 @@
-import React, {useState} from 'react';
+import React, {useState, useReducer} from 'react';
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+    switch (action.type) {
+        case 'add_blogPost':
+            return [...state, {title: 'Vue Js'}]
+    
+        default:
+            return state;
+    }
+}
 
 export const BlogProvider = ({children}) => {
-    const [blogPost, setBlogPost] = useState([{title:'React Native'}, {title:'TypeScript'}])
+    // const [blogPost, setBlogPost] = useState([{title:'React Native'}, {title:'TypeScript'}])
+    const [blogPost, dispatch] = useReducer(blogReducer, [{title:'React Native'}, {title:'TypeScript'}])
 
     const addBlogPost = () => {
-        setBlogPost([...blogPost, {title: 'Vue Js'}])
+        // setBlogPost([...blogPost, {title: 'Vue Js'}])
+        dispatch({type: 'add_blogPost'})
     }
 
     return <BlogContext.Provider value={{
