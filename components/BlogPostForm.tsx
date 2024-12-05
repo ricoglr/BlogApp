8d52aae2adaf9287674bbb8035bdf6b0e4db547e
ricoglr <rico.dev.tr@gmail.com>
2024-12-05 18:00:27 +0300

    Blog oluşturma ekranına css verilmesi

--- components/BlogPostForm.tsx
+++ components/BlogPostForm.tsx
@@ -5,18 +5,51 @@ const BlogPostForm = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
   return (
-    <View>
-      <Text>Başlığı giriniz: </Text>
-      <TextInput value={title} onChangeText={(text)=> setTitle(text)}/>
-      <Text>İçeriği giriniz: </Text>
-      <TextInput value={content} onChangeText={(text)=> setContent(text)}/>
-      <TouchableOpacity>
-        <Text>Kaydet</Text>
-      </TouchableOpacity>
+    <View style={styles.main}>
+      <Text style={styles.label}>Başlığı giriniz: </Text>
+      <TextInput style={styles.input} value={title} onChangeText={(text)=> setTitle(text)}/>
+
+      <Text style={styles.label}>İçeriği giriniz: </Text>
+      <TextInput style={styles.input} value={content} onChangeText={(text)=> setContent(text)}/>
+      <View style={styles.buttonMain}>
+        <TouchableOpacity style={styles.buttonView}>
+          <Text style={styles.buttonText}>Kaydet</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
 
 export default BlogPostForm
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  main: {
+    marginTop: 10,
+  },
+  label: {
+    fontSize: 20,
+    marginLeft: 10
+  },
+  input: {
+   borderWidth: 1,
+   margin: 10,
+   borderRadius: 10,
+   padding: 5,
+   fontSize: 18,
+   marginBottom: 15
+  },
+  buttonMain:{
+    padding: 30
+  },
+  buttonView:{
+    backgroundColor: 'brown',
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20
+  },
+  buttonText:{
+    color: "white",
+    fontSize: 20
+  }
+})
